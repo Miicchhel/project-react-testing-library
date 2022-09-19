7552c083d92487955f9ced50c2f987e23b4df7ef
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-09-19 16:01:38 -0300

    Testando o componente About.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../pages';
+
+test('Testando se a página contém um heading h2 com o texto "About Pokédex"', () => {
+  renderWithRouter(<About />);
+
+  const aboutTitle = screen.getByRole('heading', {
+    level: 2, name: /about pokédex/i,
+  });
+  expect(aboutTitle).toBeInTheDocument();
+});
+
+test('Testando se a página contém a imagem de uma Pokédex', () => {
+  renderWithRouter(<About />);
+
+  const aboutImg = screen.getByRole('img', { name: /pokédex/i });
+  expect(aboutImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
