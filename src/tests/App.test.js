59aa41ad82c5730e28b72863556390a685941720
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-09-19 15:21:27 -0300

    Testando o componente App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('testando se no topo da aplicação há um conjunto fixo de links de navegação', () => {
+  renderWithRouter(<App />);
+
+  const homeLink = screen.getByRole('link', { name: /home/i });
+  expect(homeLink).toBeInTheDocument();
+
+  const aboutLink = screen.getByRole('link', { name: /about/i });
+  expect(aboutLink).toBeInTheDocument();
+
+  const favoriteLink = screen.getByRole('link', { name: /favorite pokémon/i });
+  expect(favoriteLink).toBeInTheDocument();
+});
