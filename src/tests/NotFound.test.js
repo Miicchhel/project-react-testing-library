d9f72528f2212cc529d877c26b8b493e54b47c26
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-09-19 16:26:56 -0300

    Testando o component NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+test('Teste o componente <NotFound.js />', () => {
+  renderWithRouter(<NotFound />);
+
+  const testHeader = screen.getByRole('heading', {
+    level: 2, name: /page requested not found/i,
+  });
+  expect(testHeader).toBeInTheDocument();
+
+  const testImg = screen.getByRole('img');
+  expect(testImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
