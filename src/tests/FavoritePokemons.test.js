cfe557743aa662ceb9d0285eaaeae9308358f463
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-09-19 16:13:23 -0300

    testando o componente FavoritePokemons.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../pages';
+
+test('Teste o componente <FavoritePokemons.js />', () => {
+  renderWithRouter(<FavoritePokemons />);
+
+  const text = screen.getByText(/no favorite pokemon found/i);
+  expect(text).toBeInTheDocument();
+});
